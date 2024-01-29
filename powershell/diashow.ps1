5053b859425bdaa2f35754ba31d6dc4d64132589
ambrecht <tino@ambrecht.de>
2024-01-29 18:02:59 +0100

    DatabaseChange

--- powershell/diashow.ps1
+++
@@ -1,8 +0,0 @@
-$folderPath = 'H:\SCUMai' # Bitte ersetze 'Pfad_zu_deinem_Ordner' mit dem tatsächlichen Pfad
-$images = Get-ChildItem -Path $folderPath -Recurse -Include *.jpg,*.png,*.gif,*.bmp | Select-Object -ExpandProperty FullName
-
-while ($true) {
-    $randomImage = Get-Random -InputObject $images
-    Start-Process 'rundll32.exe' -ArgumentList "C:\WINDOWS\System32\shimgvw.dll,ImageView_Fullscreen $randomImage"
-    Start-Sleep -Seconds 2
-}
